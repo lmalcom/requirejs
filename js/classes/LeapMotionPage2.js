7385192d953dd47430168243defc303a70a9166b
Lerenzo <lerenzo@gmail.com>
2013-12-29 21:06:44 -0600

    nothin

--- js/classes/LeapMotionPage2.js
+++ js/classes/LeapMotionPage2.js
@@ -45,7 +45,6 @@ define(['LeapMotionPage'], function(LMP){
 
 			//create hand blocks
 			_.each(frame.hands, function(hand){
-				console.log(hand); 
 				var handPos = page.getHandPos(hand); 
 				page.updateBlock(handPos);
 			}); 
@@ -87,7 +86,7 @@ define(['LeapMotionPage'], function(LMP){
 			this.renderer = new THREE.CanvasRenderer(); 
 			this.renderer.setSize( window.innerWidth, window.innerHeight );
 			this.renderer.domElement.id = 'ARPageRenderer'; 
-			this.renderer.autoClear = false; 
+			//this.renderer.autoClear = false; 
 
 			// setup lights
 			this.scene.add(new THREE.AmbientLight(0xffffff));
@@ -138,7 +137,7 @@ define(['LeapMotionPage'], function(LMP){
 
 				// trigger the rendering 
 				if(page.renderer){ 
-					page.renderer.clear(); 
+					//page.renderer.clear(); 
 					page.renderer.render(page.scene, page.camera); 
 				} 
 				setTimeout(function(){

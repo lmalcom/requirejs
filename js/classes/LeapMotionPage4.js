f7a3febc456f1094906c6ba0070417eb54b60cc1
Lerenzo <lerenzo@gmail.com>
2014-02-18 11:38:47 -0800

    added new 3d model of sami for leap 4 page

--- js/classes/LeapMotionPage4.js
+++ js/classes/LeapMotionPage4.js
@@ -26,7 +26,7 @@ define(['LeapMotionPage3'], function(LMP){
                 if(ev.keyCode === 32 || ev.which === 32){
                     page.startScene(); 
                 }
-            })
+            }); 
 		},
 		className: LMP.prototype.className + ' LeapMotionPage4', 
 		render: function(){ 
@@ -105,7 +105,7 @@ define(['LeapMotionPage3'], function(LMP){
 			this.scene.add(light);	
 
 			//add to scene 
-			var person = this.createPerson(); 
+			/*var person = this.createPerson(); 
 			this.scene.add(person); 
 			this.playerModel = person; 
 			this.rightleg = person.children[0].children[0]; 
@@ -115,7 +115,29 @@ define(['LeapMotionPage3'], function(LMP){
 				if(child.material) child.material.color.setRGB(Math.random(), Math.random(), Math.random()); 
 			}); 			
 			person.position.setX(0); 
-			person.position.setZ(-250);  
+			person.position.setZ(-250);  */
+
+			/*var loader = new THREE.JSONLoader();
+			loader.load( '3dmodels/sami/sami.js', function ( geo ) {
+				material = new THREE.MeshLambertMaterial({color: 0xd12b2b});	
+				var mesh = new THREE.Mesh(geo, material); 
+				mesh.position.z = -250;
+				mesh.rotation.z = 180; 
+				page.scene.add( mesh );
+				page.playerModel = mesh; 
+			} );*/
+
+			var loader = new THREE.JSONLoader();
+			loader.load( '3dmodels/sami2/sami2.js', function ( geo, material ) {
+				console.log(geo, material); 	
+				var mesh = new THREE.Mesh(geo, new THREE.MeshFaceMaterial( material )); 
+				mesh.position.z = -250;
+				mesh.rotation.z = 180; 
+				mesh.rotation.set(30,0,180); 
+				mesh.scale.set(10,10,10); 
+				page.scene.add( mesh );
+				page.playerModel = mesh; 
+			} );
 
 			//scene 2 
 			var scene2 = page.scene2 = new THREE.Object3D(); 
@@ -170,7 +192,90 @@ define(['LeapMotionPage3'], function(LMP){
 				})
 				.start()
 			if(!page.animating) page.start();  					
-		}
+		}, 
+		start: function(){ 
+			var page = this; 
+			page.animating = true; 
+			page.playerModel.position.set(0, 0 , -250); 
+
+			//move camera 3 times 
+			setTimeout(function(){
+				page.camera.position.set(Math.random()*-500, Math.random()*500, Math.random()*1000); 
+				page.camera.lookAt(new THREE.Vector3(page.playerModel.position.x, page.playerModel.position.y, page.playerModel.position.z)); 
+			}, 1000); 
+
+			setTimeout(function(){
+				page.camera.position.set(Math.random()*500, Math.random()*-1000, Math.random()*1000); 
+				page.camera.lookAt(new THREE.Vector3(page.playerModel.position.x, page.playerModel.position.y, page.playerModel.position.z)); 
+			}, 3000); 
+
+			setTimeout(function(){
+				page.camera.position.set(Math.random()*-1500, Math.random()*500, Math.random()*1000); 
+				/*page.playerModel.position.setX(Math.random()*-500); 
+				page.playerModel.position.setY(Math.random()*500); */
+				page.camera.lookAt(new THREE.Vector3(page.playerModel.position.x, page.playerModel.position.y, page.playerModel.position.z)); 
+				//page.camera.target.position.copy( page.playerModel.position ); 
+			}, 4000); 
+
+			//camera tween 
+			var start = Date.now()/1000; 
+			var from = { 
+				angle: 0, 
+				camera: 0, 
+			}; 
+			var to = { 
+				angle: 4*Math.PI, 
+				camera: 1700, 
+			}; 
+			var tween = page.tween3 = new TWEEN.Tween(from).to(to, 20000)
+				.onStart(function(){ 
+					page.scene2Animating = true; 
+				})
+				.onUpdate(function(){ 
+					//move camera from -200 to 1000 
+					var radius = 900; 
+					page.playerModel.position.setY(from.camera + 50); 
+					//page.camera.position.set(-750, from.camera, 0); 
+					page.camera.position.set(-250 + radius*Math.cos(from.angle), from.camera, -250 + radius*Math.sin(from.angle));
+					page.camera.lookAt(new THREE.Vector3(page.scene2.position.x, page.playerModel.position.y, page.scene2.position.z)); 
+
+
+					//move 
+					//var currentTime = Date.now()/1000 - start; 
+				}) 
+				.onComplete(function(){ 
+					page.playerModel.position.setY(from.camera + 50); 
+					page.animating = false; 
+					page.scene2Animating = false; 
+				}) 
+				.delay(4000)
+				.start(); 
+
+			//space tween 
+			var currentColor = page.playerModel.color; 
+			var from2 = {
+				z: 0
+			};  
+			var tween2 = page.tween = new TWEEN.Tween(from2).to({
+					z: Math.PI*2, 
+					r: Math.random(), 
+					g: Math.random(), 
+					b: Math.random()
+				}, 1000)
+				.onStart(function(){
+					
+				})
+				.onUpdate(function(){ 
+					page.playerModel.rotateZ(from2.z); 			
+				})
+				.onComplete(function(){
+					page.playSound();
+				})
+				.delay(10000)
+				.start();  
+			return this; 
+		},
+		moveLegs: function(){ }, 
 	});  
 	return LeapMotionPage4; 
 }); 
